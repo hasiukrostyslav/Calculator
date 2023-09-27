e8b7f5c55598e5469e78d5e74eadf2bb9be21499
Rostyslav <hasiukrostyslav@gmail.com>
2023-09-27 14:22:55 +0300

    changed data

--- src/data.ts
+++ src/data.ts
@@ -1,1 +1,9 @@
-export const DIGITS: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+interface MathButtons {
+  mathButtons: string[];
+  digit: number[];
+}
+
+export const BUTTONS: MathButtons = {
+  mathButtons: ["/", "*", "-", "+", "="],
+  digit: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+};
