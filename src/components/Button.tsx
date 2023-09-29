5f8ca0b38591c1eb1357eb07e68102f2606b892f
Rostyslav <hasiukrostyslav@gmail.com>
2023-09-29 11:41:00 +0300

    finish styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-interface Props {
+interface ButtonProps {
   children: React.ReactNode;
-  type: string;
+  type: "primary" | "digit" | "math";
   id?: string | number;
   large?: boolean;
 }
 
-function Button({ type, large, children }: Props) {
-  const base = `h-[60px] w-[62px] rounded-full  border border-solid text-3xl font-medium hover:opacity-90 active:opacity-80 flex items-center hover:transition-all`;
+function Button({ type, large, children }: ButtonProps) {
+  const base = `h-[60px] rounded-full  border border-solid text-3xl font-medium hover:opacity-90 active:opacity-80 flex items-center hover:transition-all`;
 
   const styles = {
-    primary: `${base} text-primary-950 bg-primary-400 border-primary-400 justify-center text-[28px]`,
+    primary: `${base} text-primary-950 bg-primary-400 border-primary-400 justify-center text-[28px] w-[62px]`,
     digit: `${base} text-primary-400 bg-primary-700 border-primary-700 last:mt-3.5 ${
-      large ? "mt-3.5 w-36 px-5 text-left" : "justify-center"
+      large ? "mt-3.5 w-36 px-5 text-left" : "justify-center w-[62px]"
     }`,
-    math: `${base} justify-center text-primary-100 bg-accent border-accent first:mb-1.5 last:mt-3.5`,
+    math: `${base} justify-center text-primary-100 bg-accent border-accent first:mb-1.5 last:mt-3.5 w-[62px]`,
   };
 
   return <button className={styles[type]}>{children}</button>;
